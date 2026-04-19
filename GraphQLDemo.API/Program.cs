ed7c7182e6e561fed7c44699969e54a8bdd182d3
SebastianLai <surregograciano@gmail.com>
2026-04-19 11:58:29 -0500

    Agregar archivos de proyecto.

---
+++ GraphQLDemo.API/Program.cs
@@ -0,0 +1,29 @@
+using GraphQLDemo.API.Schema;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddGraphQLServer().AddQueryType<Query>(); 
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+//app.MapControllers();
+
+app.MapGraphQL(); 
+
+app.Run();
---
+++ GraphQLDemo.API/Schema/Query.cs
@@ -0,0 +1,7 @@
+﻿namespace GraphQLDemo.API.Schema
+{
+    public class Query
+    {
+        public string Instructions => "Hola que más como estan todos";
+    }
+}
